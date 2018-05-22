303c574b9515e1405334739e483982a628d4e76f
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-22 16:00:10 -0500

    intento 6

--- public/javascripts/gaugedraw.js
+++ public/javascripts/gaugedraw.js
@@ -1,28 +1,26 @@
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
+  	
+	google.charts.load('current', {'packages':['gauge']});
+      	google.charts.setOnLoadCallback(drawChart);
+        var tem = document.getElementById("index").temperatureData;
+	function drawChart() 
+	{	
+        	var data = google.visualization.arrayToDataTable([
+          	['Label', 'Value'],
+          	['tempertatura', 0]
+        	]);
 
-	function drawChart() {
-	
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['tempertatura', 0]
-        ]);
-
-        var options = {
-          width: 400, height: 200,         
-	  redFrom: 60, redTo: 100,
-          yellowFrom:0, yellowTo: 60,
-          minorTicks: 5
-        };
-	
-
+        	var options = 
+		{
+          	width: 400, height: 200,         
+	  	redFrom: 60, redTo: 100,
+          	yellowFrom:0, yellowTo: 60,
+          	minorTicks: 5
+        	};
         var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 	chart.draw(data, options);
-	  setInterval(function() {
-	    data: temperatureData
-	    dataType: 'json'	
-	    var obj = JSON.parse(temperatura.Data)
-            data.setValue(0, 1, obj[0].temperature);
-          chart.draw(data, options);
-        }, 1300)
-      }
+		setInterval(function() 
+		{
+	    	data.setValue(0, 1, tem);
+          	chart.draw(data, options);
+        	}, 1300)
+      	}
