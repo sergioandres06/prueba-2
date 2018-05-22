52d2de6d8e987f5a8a185d8b0907c1e639e0a383
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-22 08:22:47 -0500

    intento4

---
+++ public/javascripts/gaugedraw.js
@@ -0,0 +1,26 @@
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+	var temperatureData = [];
+	function drawChart() {
+	
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['tempertatura', 0]
+        ]);
+
+        var options = {
+          width: 400, height: 200,         
+	  redFrom: 60, redTo: 100,
+          yellowFrom:0, yellowTo: 60,
+          minorTicks: 5
+        };
+	
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+	chart.draw(data, options);
+	  setInterval(function() {
+            data.setValue(0, 1, temperatureData);
+          chart.draw(data, options);
+        }, 1300)
+      }
