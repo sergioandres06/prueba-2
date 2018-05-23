5efa3792c26a9f3871dfbb9eafb240f55be4cd72
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-23 10:46:16 -0500

    si

--- public/javascripts/gaugedraw.js
+++ public/javascripts/gaugedraw.js
@@ -1,7 +1,7 @@
   	
 	google.charts.load('current', {'packages':['gauge']});
       	google.charts.setOnLoadCallback(drawChart);
-        
+        var temperatureData=[];
 	function drawChart() 
 	{	
         	var data = google.visualization.arrayToDataTable([
@@ -19,13 +19,27 @@
         var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 	chart.draw(data, options);
 		setInterval(function() 
-		{
-		var JSON=$.ajax({
-		url:'file:///javascripts/index.js',
-		dataType: 'json',
-		async: false}).responseValue;
-		var tem = jQuery.parseJSON(JSON);
-	    	data.setValue(0, 1, tem[0].temperature);
+		{		
+	    	data.setValue(0, 1, tem);
           	chart.draw(data, options);
         	}, 1300)
-      	}
+		try 
+			{
+      			var obj = JSON.parse(message.data);
+      			if(!obj.temperature) 
+				{
+        			return;
+      				}
+      			temperatureData.push(obj.temperature); 
+				{
+        			temperatureData.shift();
+      				}
+      			Chart.update();
+			} 
+		catch (err) 
+				{
+      				console.error(err);
+    				}
+			}
+
+      	
