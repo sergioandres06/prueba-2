60a067861626416f962ea0195bed7353aad75da9
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-24 14:17:21 -0500

    intento 17

--- public/javascripts/gaugedraw.js
+++ public/javascripts/gaugedraw.js
@@ -6,19 +6,7 @@
 	ws.onmessage = function (message) 
 		{
     		console.log('receive message' + message.data);
-		try
-		{
-		var temp = JSON.parse(message.data);
-		if(!temp.temperature)
-		{
-		return;
-		}
-			
-		}
-		catch(err)
-		{
-		console.error(err);
-		}}
+	}
 	function drawChart() 
 	{	
         	var data = google.visualization.arrayToDataTable([
@@ -36,7 +24,9 @@
         var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 	chart.draw(data, options);
 		setInterval(function() 
-		{		
+		{
+		var temp =JSON.parse(message.data); 
+				
 	    	data.setValue(0, 1, temp[0].temperature);
           	chart.draw(data, options);
         	}, 1300);
