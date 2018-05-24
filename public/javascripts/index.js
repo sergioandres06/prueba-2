7214fdaf5b320eb107208fd86ea863f7f74298ca
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-24 16:25:28 -0500

    intento 22

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -239,9 +239,9 @@ $(document).ready(function ()
 	chart.draw(data3, options);
 		setInterval(function() 
 		{
-		 
+		var tem = jQuery.parseJSON(mensaje.data);
 				
-	    	data.setValue(0, 1, obj.temperature);
+	    	data.setValue(0, 1, tem[0].temperature);
           	chart.draw(data3, options);
         	}, 1300);
 	}
