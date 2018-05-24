6336eab9b3b820d09326ae64564029bb88a5d5c6
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-24 14:55:35 -0500

    intento 20

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -216,6 +216,37 @@ $(document).ready(function ()
 			{
 			console.error(err);
 			}
+
+	google.charts.load('current', {'packages':['gauge']});
+      	google.charts.setOnLoadCallback(drawChart);
+        
+	 	
+	function drawChart() 
+		{	
+        	var data3 = google.visualization.arrayToDataTable([
+          	['Label', 'Value'],
+          	['tempertatura', 0]
+        	]);
+
+        	var options = 
+		{
+          	width: 400, height: 200,         
+	  	redFrom: 60, redTo: 100,
+          	yellowFrom:0, yellowTo: 60,
+          	minorTicks: 5
+        	};
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+	chart.draw(data3, options);
+		setInterval(function() 
+		{
+		 
+				
+	    	data.setValue(0, 1, temperatureData);
+          	chart.draw(data3, options);
+        	}, 1300);
+	}
    		}
+
+
        
 });
