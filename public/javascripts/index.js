702b3919bf4aff5f0f38d6e25e438cfa2d33efc0
Sergio Saavedra <sergio.saavedra.060793@gmail.com>
2018-05-28 12:43:44 -0500

    yalisto

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -276,7 +276,7 @@ $(document).ready(function ()
           	['tempertatura', tem2]
         	]);
 
-        	var options = 
+        	var options2 = 
 		{
           	width: 400, height: 200,         
 	  	redFrom: 60, redTo: 100,
@@ -284,7 +284,7 @@ $(document).ready(function ()
           	minorTicks: 5
         	};
         var chart2 = new google.visualization.Gauge(document.getElementById('chart_div2'));
-	chart.draw(data4, options);
+	chart.draw(data4, options2);
 		try 
 			{
       			var obj = JSON.parse(message.data);
@@ -299,7 +299,7 @@ $(document).ready(function ()
           			['Label', 'Value'],
           			['tempertatura', tem2]
         			]);
-			chart.draw(data4, options);
+			chart.draw(data4, options2);
       				}
 			
     			} 
